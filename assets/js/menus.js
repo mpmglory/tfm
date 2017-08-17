bded8b68a2707bc7da1787f7659846b778022053
platinnson <joyplatini@gmail.com>
2017-08-17 16:36:01 +0100

    modif des textos du 16-08-2017

---
+++ assets/js/menus.js
@@ -0,0 +1,25 @@
+$('#header').html('<header id="topNav"><div class="container">\
+                        <button class="btn btn-mobile" data-toggle="collapse" data-target=".nav-main-collapse"><i class="fa fa-bars"></i></button>\
+                        <a class="logo pull-left" href="index.html"><img src="assets/images/demo/brands/7.jpg" alt="" /></a>\
+                        <div class="navbar-collapse pull-right nav-main-collapse collapse submenu-dark"><nav class="nav-main"><ul id="topMain" class="nav nav-pills nav-main">\
+                                        <li class="dropdown active"><a href="index.html">ACCUEIL</a></li>\
+                                        <li class="dropdown"><a class="dropdown-toggle" href="page-presentation.html">A PROPOS DE NOUS</a>\
+                                            <ul class="dropdown-menu nav nav-pills nav-main nav-onepage">\
+                                                <li class="dropdown"><a href="page-presentation.html">Présentation</a></li>\
+								                <li class="dropdown"><a href="page-equipe.html">Notre équipe</a></li>\
+											    <li class="dropdown"><a href="page-clients.html">Nos clients</a></li>\
+											 </ul>\
+                                        <li class="dropdown"><a class="dropdown-toggle" href="page-services.html">NOS SERVICES</a><ul class="dropdown-menu nav nav-pills nav-main nav-onepage">\
+                                                <li class="dropdown"><a href="page-ser-gestion-forestiere.html #list-ser">Gestion Forestière</a></li>\
+								                <li class="dropdown"><a href="page-ser-expertise.html #list-ser">Expertise Environnementale</a></li>\
+											    <li class="dropdown"><a href="page-ser-carto-mode.html #list-ser">Cartographie et Modélisation</a></li>\
+											    <li id="ser-ib" class="dropdown"><a href="page-ser-ib.html #list-ser">Industrie du bois</a></li>\
+											    <li class="dropdown"><a href="page-ser-formation.html #list-ser">Formation</a></li>\
+											    <li id="ser-appui" class="dropdown"><a href="page-ser-appui.html #list-ser">Appui et Gestion</a></li>\
+                                                <li class="dropdown"><a href="page-ser-chang-clim.html #list-ser">Changement climatique</a></li></ul></li>\
+                                        <li><a href="page-partenaires.html">NOS PARTENAIRES</a></li>\
+                                        <li><a href="page-coming-soon-2.html">REFERENCES</a></li>\
+                                        <li><a href="page-contact.html">NOUS CONTACTER</a></li>\
+</ul></nav></div></div></header>'
+);
+
