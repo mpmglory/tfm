6591922d4e599ebedef690d1b0c0c0a493fe986d
platinnson <joyplatini@gmail.com>
2017-08-27 14:53:00 +0100

    MAj dimanche 27/08 pour push

---
+++ garde-menu.js
@@ -0,0 +1,8 @@
+<ul class="dropdown-menu nav nav-pills nav-main nav-onepage">\
+                                                <li class="dropdown"><a href="page-ser-gestion-forestiere.html #list-ser">Gestion Forestière</a></li>\
+								                <li class="dropdown"><a href="page-ser-expertise.html #list-ser">Expertise Environnementale</a></li>\
+											    <li class="dropdown"><a href="page-ser-carto-mode.html #list-ser">Cartographie et Modélisation</a></li>\
+											    <li id="ser-ib" class="dropdown"><a href="page-ser-ib.html #list-ser">Industrie du bois</a></li>\
+											    <li class="dropdown"><a href="page-ser-formation.html #list-ser">Formation</a></li>\
+											    <li id="ser-appui" class="dropdown"><a href="page-ser-appui.html #list-ser">Appui et Gestion</a></li>\
+                                                <li class="dropdown"><a href="page-ser-chang-clim.html #list-ser">Changement climatique</a></li></ul></li>\
