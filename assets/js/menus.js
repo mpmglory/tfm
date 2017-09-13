edbb00c845ebdf92e0e79fb5e733c754e68e55a4
PENKA <maggwilly03@gmail.com>
2017-09-13 13:01:42 +0100

    les modifs

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -3,14 +3,14 @@ $('#header').html('<header id="topNav"><div class="container">\
                         <a class="logo pull-left" href="index.html"><img src="assets/images/demo/brands/7.jpg" alt="" /></a>\
                         <div class="navbar-collapse pull-right nav-main-collapse collapse submenu-dark"><nav class="nav-main"><ul id="topMain" class="nav nav-pills nav-main">\
                                         <li class="active"><a href="index.html">ACCUEIL</a></li>\
-                                        <li class="dropdown"><a class="dropdown-toggle" href="page-presentation.html">A PROPOS DE NOUS</a>\
+										<li><a href="page-services.html">NOS SERVICES</a></li>\
+                                        <li class="dropdown"><a class="dropdown-toggle" href="#">A PROPOS DE NOUS</a>\
                                             <ul class="dropdown-menu nav nav-pills nav-main nav-onepage">\
                                                 <li class="dropdown"><a href="page-presentation.html">Présentation</a></li>\
 								                <li class="dropdown"><a href="page-politique.html">Notre politique</a></li>\
                                                 <li class="dropdown"><a href="page-equipe.html">Notre équipe</a></li>\
 											    <li class="dropdown"><a href="page-clients.html">Nos clients</a></li>\
-											 </ul>\
-                                        <li><a href="page-services.html">NOS SERVICES</a>\
+											 </ul></li>\
                                         <li><a href="page-partenaires.html">NOS PARTENAIRES</a></li>\
                                         <li><a href="page-coming-soon-2.html">REFERENCES</a></li>\
                                         <li><a href="page-contact.html">NOUS CONTACTER</a></li>\
